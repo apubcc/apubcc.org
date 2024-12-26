469d8bdb92980eb0ba7ef36fe06e7e879b76dc0a
AaronTan11 <tanweihup@gmail.com>
2024-12-26 20:21:38 +0800

    refactor: adjust Hero component image to avoid width overflow

--- components/pages-sections/Hero.tsx
+++ components/pages-sections/Hero.tsx
@@ -36,7 +36,7 @@ export default function Hero() {
 
         <main className="pl-[1.1875rem] pr-[7rem] pt-[1.875rem] lg:px-[4.6875rem] lg:pt-[100px] relative">
           <h1 className="text-black relative z-20 text-xl lg:text-[3.4375rem] font-extrabold w-[13.1875rem] lg:w-[37.75rem] tracking-[1.1px] text-stroke-1 text-stroke-black">LEADING SOUTHEAST ASIA STUDENT BLOCKCHAIN CLUB</h1>
-          <img src="/fireball.png" alt="fireball image" className="absolute z-10 top-0 left-32 lg:left-96 w-[200px] h-[200px] lg:w-[668px] lg:h-[668px] xl:-top-10 xl:left-[350px]"/>
+          <img src="/fireball.png" alt="fireball image" className="absolute z-10 top-5 left-36 lg:left-96 w-[9.5rem] h-[9.5rem] lg:w-[668px] lg:h-[668px] xl:-top-10 xl:left-[350px]"/>
         </main> 
       </div>
     </div> 
