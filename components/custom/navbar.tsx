35386b9b674382d28f6321b38d6823f8ce151b2e
AaronTan11 <tanweihup@gmail.com>
2025-01-23 17:55:56 +0800

    feat: updated NavBarItem to accept href
    - Added Link component from next-view-transitions to NavbarItem for navigation.
    - Updated NavbarItemProps to include href property for link functionality.

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 
 import { cn } from "@/lib/utils"
-
+import { Link } from "next-view-transitions"
 
 export interface NavbarProps
   extends React.ButtonHTMLAttributes<HTMLDivElement> {
@@ -11,6 +11,7 @@ export interface NavbarProps
 
 export interface NavbarItemProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  href: string
   asChild?: boolean
 }
 
@@ -29,14 +30,16 @@ const Navbar = React.forwardRef<HTMLElement, NavbarProps>(
 Navbar.displayName = "Navbar"
 
 const NavbarItem = React.forwardRef<HTMLButtonElement, NavbarItemProps>(
-  ({ className, asChild = false, ...props }, ref) => {
+  ({ href, className, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
-      <Comp
-        className={cn(className, "text-white text-xl font-bold leading-normal")}
-        ref={ref}
-        {...props}
-      />
+      <Link href={href}>
+        <Comp
+          className={cn(className, "text-white text-xl font-bold leading-normal")}
+          ref={ref}
+          {...props}
+        />
+      </Link>
     )
   }
 )
