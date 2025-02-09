ef12af8d2699df484de8c12b57152dfcdfe1689c
AaronTan11 <tanweihup@gmail.com>
2025-02-09 23:21:21 +0800

    changed hero again, probably fked up respon

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ import VisionAndMission from "@/components/home/VisionsMissions";
 export default function Home() {
   return (
     <>
-      <section className="px-[0.875rem] py-[1.8125rem] md:px-20 lg:py-[1.375rem] max-w-6xl mx-auto">
+      <section className="px-[0.875rem] py-[1.8125rem] md:px-20 lg:px-32 lg:py-[3.5rem]">
         <Hero />
       </section> 
 
-      <section className="px-[0.875rem] py-[1.8125rem] md:py-24 lg:px-20 lg:pt-28 max-w-6xl mx-auto">
+      {/* <section className="px-[0.875rem] py-[1.8125rem] md:py-24 lg:px-20 lg:pt-28 max-w-6xl mx-auto">
         <Achievements />
       </section>
 
@@ -32,7 +32,7 @@ export default function Home() {
       
       <section className="pt-8 md:pt-20 lg:pt-28 lg:px-20 h-[68svh] justify-center flex items-end bg-gradient-to-b from-[rgba(238,145,89,0)] from-[17.28%] to-[#F16109] to-[73%]">
         <Footer />
-      </section>
+      </section> */}
 
     </>
   );
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -61,6 +61,7 @@ export default function Hero() {
     // Add a continuous floating animation
     gsap.to(fireballRef.current, {
       duration: 2,
+      x: "+=30",
       y: "+=20",
       rotation: 5,
       repeat: -1,
@@ -71,28 +72,28 @@ export default function Hero() {
   }, { scope: containerRef });
 
   return (
-    <div ref={containerRef} className="max-w-7xl h-[12.75rem] md:h-[22rem] lg:h-[28rem] rounded-3xl bg-[url('/hero-mobile-bg.png')] lg:bg-[url('/hero-desktop-bg.png')] bg-cover flex flex-col relative">
-      <header className="flex gap-[7px] md:h-16 xl:h-20 items-center">
-        <section className="w-[102px] md:w-[180px] lg:w-[200px] xl:w-[250px]">
+    <div ref={containerRef} className="aspect-[35/16] rounded-3xl bg-[url('/hero-mobile-bg.png')] lg:bg-[url('/hero-desktop-bg.png')] bg-cover flex flex-col relative">
+      <header className="flex gap-[7px] md:h-16 xl:h-32 items-center justify-center">
+        <section className="w-[102px] md:w-[180px] lg:w-[200px] xl:w-[28rem]">
           <img src="/apubcc-horizontal.png" alt="apubcc horizontal logo" className="w-full h-full object-contain" />
         </section>
 
-        <section className="flex items-center gap-3 justify-end w-full pr-[10px]">
-          <Navbar className="lg:w-[33rem] lg:h-[2.69rem] xl:w-[38rem] xl:h-[2.5rem] lg:flex hidden">
+        <section className="flex items-center gap-5 justify-end w-full pr-[10px] pl-10">
+          <Navbar className="lg:w-[33rem] lg:h-[2.69rem] xl:w-full xl:h-[3.5rem] lg:flex hidden">
             <NavbarItem href="/events">Events</NavbarItem>
             <NavbarItem href="/resources">Resources</NavbarItem>
             <NavbarItem href="/about-us">About Us</NavbarItem>
             <NavbarItem href="/contact-us">Contact Us</NavbarItem>
           </Navbar>
 
-          <Button className="lg:flex hidden bg-white text-black text-[1.3125rem] font-bold leading-normal rounded-[20px] border border-[#FF2200] shadow-[0_0_30px_0_#FF2200]">
+          <Button className="lg:flex hidden bg-white text-black text-[1.3125rem] font-bold leading-normal rounded-[20px] border border-[#FF2200] shadow-[0_0_30px_0_#FF2200] py-6">
             Join Us
           </Button>
         </section>
       </header>
 
-      <main className="relative flex-1 flex items-center px-6">
-        <h1 className="text-black text-xl md:text-5xl lg:text-[3.4375rem] font-extrabold w-[13.1875rem] md:w-[30rem] lg:w-[37.75rem] tracking-[1.1px] text-stroke-1 text-stroke-black relative z-10">
+      <main className="relative flex-1 flex items-center px-6 xl:px-[5rem]">
+        <h1 className="text-black text-xl md:text-5xl lg:text-[3.4375rem] xl:text-[4.5rem] xl:leading-[6rem] xl:w-[45rem] font-extrabold w-[13.1875rem] md:w-[30rem] lg:w-[37.75rem] tracking-[1.1px] text-stroke-1 text-stroke-black relative z-10">
           LEADING SOUTHEAST ASIA STUDENT BLOCKCHAIN CLUB
         </h1>
 
@@ -100,13 +101,13 @@ export default function Hero() {
           ref={fireballRef}
           src="/fireball.png"
           alt="fireball image"
-          className="fixed right-[30rem] top-32 transform z-[1]
+          className="fixed right-[30rem] xl:right-[20rem] top-[13rem] transform z-[1]
             w-[150px] h-[150px]    /* smallest screens */
             min-[425px]:w-[200px] min-[425px]:h-[200px]    /* small screens */
             sm:w-[250px] sm:h-[250px]    /* small screens */
             md:w-[280px] md:h-[280px]    /* medium screens */
             lg:w-[350px] lg:h-[350px]    /* large screens */
-            xl:w-[400px] xl:h-[400px]    /* extra large screens */
+            xl:w-[39rem] xl:h-[39rem]    /* extra large screens */
             object-contain"               /* maintains aspect ratio */
         />
       </main>
