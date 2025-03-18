8a0f3e8f9a0a1e960ff80da356f86b981ac860b9
AaronTan11 <tanweihup@gmail.com>
2025-03-18 15:28:00 +0800

    wrapped the button as link

--- src/components/home/EventCard.tsx
+++ src/components/home/EventCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function EventCard({ event }: { event: any }) {
 	return (
@@ -14,10 +15,10 @@ export default function EventCard({ event }: { event: any }) {
 				<h3 className="text-[0.75rem] font-bold lg:text-[1rem]">{event.name}</h3>
 				<p className="text-[0.625rem] font-bold lg:text-[0.8rem]">{event.date}</p>
 
-				<Button className="w-full bg-[#82380B] text-white rounded-[0.375rem] h-[1.125rem] text-[0.5625rem] mt-[0.75rem] lg:h-[1.5rem] lg:text-[1rem]">
-					<a href={event.link} target="_blank" rel="noopener noreferrer">
+				<Button asChild className="w-full bg-[#82380B] text-white rounded-[0.375rem] h-[1.125rem] text-[0.5625rem] mt-[0.75rem] lg:h-[1.5rem] lg:text-[1rem]">
+					<Link href={event.link} target="_blank" rel="noopener noreferrer">
 						Book Ticket
-					</a>
+					</Link>
 				</Button>
 			</div>
 		</div>
